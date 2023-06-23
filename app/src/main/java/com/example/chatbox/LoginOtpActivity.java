46226b9e14bacc908716c2acf36f98049283a8ab
Gaurav Vishwakarma <gauravvishwakamra071@gmail.com>
2023-06-23 15:31:37 +0530

    This Is ChatBox Commit which Is A Chatting App Project.

--- app/src/main/java/com/example/chatbox/LoginOtpActivity.java
+++ app/src/main/java/com/example/chatbox/LoginOtpActivity.java
@@ -1,19 +1,107 @@
 package com.example.chatbox;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.chatbox.utils.AndroidUtil;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class LoginOtpActivity extends AppCompatActivity {
 
     String phoneNumber;
+    Long timeOutSeconds = 60L;
+    String varification;
+    PhoneAuthProvider.ForceResendingToken resendingToken;
+    
+    EditText otpInput;
+    Button nextBtn;
+    ProgressBar progressBar;
+    TextView resendOtpTextView;
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_otp);
 
+        otpInput = findViewById(R.id.login_otp );
+        nextBtn  = findViewById(R.id.login_next_btn);
+        progressBar = findViewById(R.id.login_progress_bar);
+        resendOtpTextView = findViewById(R.id.resend_otp_textView);
+
         phoneNumber = getIntent().getExtras().getString("phone");
-        Toast.makeText(this, phoneNumber , Toast.LENGTH_SHORT).show();
+
+        sendOtp(phoneNumber,false);
+    }
+    //OTP SEND AND VARIFICATION PROCESS
+    void sendOtp(String phoneNumber,boolean isResend){
+        setInProgress(true);
+        PhoneAuthOptions.Builder builder =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber(phoneNumber)
+                        .setTimeout(timeOutSeconds, TimeUnit.SECONDS)
+                        .setActivity(this)      
+                        .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                            @Override
+                            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                                signIn(phoneAuthCredential);
+                                setInProgress(false);
+                            }
+
+                            @Override
+                            public void onVerificationFailed(@NonNull FirebaseException e) {
+                                AndroidUtil.showToast(getApplication(),"OTP varification failed");
+                                setInProgress(false);
+
+                            }
+
+                            @Override
+                            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                                super.onCodeSent(s, forceResendingToken);
+                                varification  = s;
+                                resendingToken = forceResendingToken;
+                                AndroidUtil.showToast(getApplication(),"OTP varification successfully");
+                                setInProgress(false);
+
+                            }
+                        });
+        // in case if otp resend it resends Token otherwise it verfiy phone number.
+        if(isResend){
+            PhoneAuthProvider.verifyPhoneNumber(builder.setForceResendingToken(resendingToken).build());
+        }else{
+            PhoneAuthProvider.verifyPhoneNumber(builder.build());
+        }
+        
+    }
+    //Progress bar Visibility after otp varification.
+    void setInProgress(boolean inProgress){
+        if(inProgress){
+            progressBar.setVisibility(View.VISIBLE);
+            nextBtn.setVisibility(View.GONE);
+        }else{
+            progressBar.setVisibility(View.GONE);
+            nextBtn.setVisibility(View.VISIBLE);
+        }
+    }
+    void signIn(PhoneAuthCredential phoneAuthCredential){
+        //login and go to next activity.
+
     }
 }
