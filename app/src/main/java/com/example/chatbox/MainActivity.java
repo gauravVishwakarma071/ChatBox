a0c4d3986c2c0ac8da8d916aa3c2d1964d3277de
Gaurav Vishwakarma <gauravvishwakamra071@gmail.com>
2023-07-07 16:45:06 +0530

    updates after Ix

--- app/src/main/java/com/example/chatbox/MainActivity.java
+++ app/src/main/java/com/example/chatbox/MainActivity.java
@@ -1,15 +1,54 @@
 package com.example.chatbox;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
-
     //New Attempt To Create ChatBox
+
+    BottomNavigationView bottomNavigationView;
+    ImageButton searchButton;
+
+    ChatFragment chatFragment;
+    ProfileFragment profileFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chatFragment = new ChatFragment();
+        profileFragment = new ProfileFragment();
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        searchButton = findViewById(R.id.main_search_btn);
+
+        searchButton.setOnClickListener(v -> {
+        startActivity(new Intent(MainActivity.this,SearchUserActivity.class));
+        });
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId()==R.id.menu_chat){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_layout,chatFragment).commit();
+                }
+                if(item.getItemId()==R.id.menu_profile){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_layout,profileFragment).commit();
+                }
+                return false;
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.menu_chat);
+
+
     }
 }
