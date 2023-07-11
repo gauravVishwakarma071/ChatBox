594603cbde0ba09d0e844f71771010f1b5c7629a
Gaurav Vishwakarma <gauravvishwakamra071@gmail.com>
2023-07-11 15:52:30 +0530

    updates after Ix

--- app/src/main/java/com/example/chatbox/MainActivity.java
+++ app/src/main/java/com/example/chatbox/MainActivity.java
@@ -4,17 +4,21 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
     //New Attempt To Create ChatBox
-
+//
     BottomNavigationView bottomNavigationView;
     ImageButton searchButton;
 
@@ -28,21 +32,28 @@ public class MainActivity extends AppCompatActivity {
         chatFragment = new ChatFragment();
         profileFragment = new ProfileFragment();
 
+
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         searchButton = findViewById(R.id.main_search_btn);
+
+
 
         searchButton.setOnClickListener(v -> {
         startActivity(new Intent(MainActivity.this,SearchUserActivity.class));
         });
+
+
 
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 if(item.getItemId()==R.id.menu_chat){
                     getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_layout,chatFragment).commit();
+
                 }
                 if(item.getItemId()==R.id.menu_profile){
                     getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_layout,profileFragment).commit();
+
                 }
                 return false;
             }
--- app/src/main/java/com/example/chatbox/SearchUserActivity.java
+++ app/src/main/java/com/example/chatbox/SearchUserActivity.java
@@ -1,14 +1,48 @@
 package com.example.chatbox;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class SearchUserActivity extends AppCompatActivity {
 
+    EditText searchInput;
+    ImageButton searchButton;
+    ImageButton backButton;
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_user);
+
+        searchInput = findViewById(R.id.search_username_input);
+        searchButton = findViewById(R.id.search_user_btn);
+        backButton = findViewById(R.id.back_btn);
+        recyclerView = findViewById(R.id.search_user_recycler_view);
+
+        searchInput.requestFocus();//last..........
+
+
+        backButton.setOnClickListener(v -> {
+           onBackPressed();
+        });
+
+        searchButton.setOnClickListener(V ->{
+            String searchTerm = searchInput.getText().toString();
+            if(searchTerm.isEmpty() || searchTerm.length()<3){
+                searchInput.setError("Invaild Username");
+                return;
+            }
+            setupSearchRecyclerView(searchTerm);
+        });
     }
+    void setupSearchRecyclerView(String searchTerm){
+
+    }
+
 }
