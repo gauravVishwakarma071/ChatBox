0d110cc4616e5f4626e11043553f382d2f49cea5
Gaurav Vishwakarma <gauravvishwakamra071@gmail.com>
2023-07-14 13:39:04 +0530

    updates after Ix

--- app/src/main/java/com/example/chatbox/MainActivity.java
+++ app/src/main/java/com/example/chatbox/MainActivity.java
@@ -11,14 +11,15 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
     //New Attempt To Create ChatBox
-//
     BottomNavigationView bottomNavigationView;
     ImageButton searchButton;
 
@@ -29,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+            Toast.makeText(this, "Still in development phase", Toast.LENGTH_SHORT).show();
+
         chatFragment = new ChatFragment();
         profileFragment = new ProfileFragment();
 
@@ -37,29 +40,27 @@ public class MainActivity extends AppCompatActivity {
         searchButton = findViewById(R.id.main_search_btn);
 
 
-
+        //Search Button Intent...
         searchButton.setOnClickListener(v -> {
         startActivity(new Intent(MainActivity.this,SearchUserActivity.class));
         });
 
 
-
+        //Botom Navigaton View...
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 if(item.getItemId()==R.id.menu_chat){
                     getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_layout,chatFragment).commit();
-
+                    Toast.makeText(MainActivity.this, "Your chats", Toast.LENGTH_SHORT).show();
                 }
                 if(item.getItemId()==R.id.menu_profile){
                     getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_layout,profileFragment).commit();
-
+                    Toast.makeText(MainActivity.this, "Your profile", Toast.LENGTH_SHORT).show();
                 }
                 return false;
             }
         });
         bottomNavigationView.setSelectedItemId(R.id.menu_chat);
-
-
     }
 }
